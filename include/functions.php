0cb6d1ce5a7cd1f3c6dd0d76d58032be8045b6d5
Ricardo Jonadabs César <pexinho@uol.com.br>
2017-08-31 01:23:35 -0300

    Functions BackMessages

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting (E_ALL & ~ E_NOTICE); // & ~ E_DEPRECATED
+error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED); //
 setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
 mb_internal_encoding('UTF-8');
