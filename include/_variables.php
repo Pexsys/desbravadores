b130c92f1a1ab6d38dbe9f4b2a5d93b290b51904
Ricardo Jonadabs César <ricardo@kapmug.com.br>
2017-11-29 16:33:10 -0200

    Bars Patten

--- include/_variables.php
+++ include/_variables.php
@@ -11,18 +11,21 @@ $pattern = array(
 	"clubeDS" => "Clube de Desbravadores Pioneiros - IASD Capão Redondo - 6ª Região - APS - UCB - DSA",
 	"virtualDir" => "/desbravadores/",
 	"bars" => array(
-		"CI" => "P",
-		"FN" => array(
-			"0" => "SIMPLES",
-			"A" => "CARTÃO DE CLASSE",
-			"B" => "CADERNO DE CLASSE",
-			"C" => "PASTA DE CLASSE",
-			"D" => "AUTORIZAÇÃO COMUM",
-			"E" => "CADERNO DE ESPECIALIDADE",
-			"F" => "AUTORIZAÇÃO ESPECIAL"
+		"CI" => array( "size" => 1, "fixed" => "P" ),
+		"FN" => array( 
+            "size" => 1, 
+            "variables" => array(
+                "0" => "SIMPLES",
+                "A" => "CARTÃO DE CLASSE",
+                "B" => "CADERNO DE CLASSE",
+                "C" => "PASTA DE CLASSE",
+                "D" => "AUTORIZAÇÃO COMUM",
+                "E" => "CADERNO DE ESPECIALIDADE",
+                "F" => "AUTORIZAÇÃO ESPECIAL"
+            )
 		),
-		"FI" => 2,
-		"NI" => 3
+		"FI" => array( "size" => 2 ),
+		"NI" => array( "size" => 3 )
 	)
 );
 ?>
