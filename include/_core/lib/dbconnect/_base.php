277768332feb9ec38f669e1a6750f1b51f0524d3
Ricardo Jonadabs César <ricardo@kapmug.com.br>
2017-12-19 17:59:38 -0200

    Connection Bugfixes

--- include/_core/lib/dbconnect/_base.php
+++ include/_core/lib/dbconnect/_base.php
@@ -1,5 +1,6 @@
 <?php
-$sa = $_SERVER["SERVER_ADMIN"];
+//$sa = $_SERVER["SERVER_ADMIN"];
+$su = $_SERVER["HTTP_X_USERNAME"];
 $sn = $_SERVER["SERVER_NAME"];
-include_once($sa."_".$sn.".ini.php");
+include_once($su."_".$sn.".ini.php");
 ?>
