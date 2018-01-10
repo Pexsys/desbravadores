a1f96899dd0d468d7d63b375dc1a4639ea69422d
Ricardo Jonadabs César <ricardo@kapmug.com.br>
2018-01-09 22:01:40 -0200

    Graph Refactoring

--- include/_patterns.php
+++ include/_patterns.php
@@ -108,7 +108,7 @@ class BARS {
 				"fi" => $a[2],
 				"ni" => $a[3]
 		);
-	}
+    }
 	
 	public function decode($s){
 		$a = $this->split($s);
@@ -117,11 +117,11 @@ class BARS {
 				"split" => $a,
 				"ci" => $a["ci"],
 				"cp" => $a["cp"],
-				"id" => base_convert($a["id"],36,10),
-				"fi" => base_convert($a["fi"],36,10),
-				"ni" => base_convert($a["ni"],36,10),
+				"id" => PATTERNS::fromConvert($a["id"]),
+				"fi" => PATTERNS::fromConvert($a["fi"]),
+				"ni" => PATTERNS::fromConvert($a["ni"]),
 		);
-	}
+    }
 	
 	public function encode($a){
 		$fn = "0";
@@ -134,12 +134,12 @@ class BARS {
 		
 		$fi = fStrZero(0,$this->getFILength());
 		if (isset($a["fi"])):
-			$fi = fStrZero(base_convert($a["fi"],10,36),$this->getFILength());
+			$fi = fStrZero(PATTERNS::toConvert($a["fi"]),$this->getFILength());
 		endif;
 		
 		$ni = fStrZero(0,$this->getNILength());
 		if (isset($a["ni"])):
-			$ni = fStrZero(base_convert($a["ni"],10,36),$this->getNILength());
+			$ni = fStrZero(PATTERNS::toConvert($a["ni"]),$this->getNILength());
 		endif;
 		
 		return mb_strtoupper($this->getClubePrefix() . $fn . $fi . $ni);
@@ -170,4 +170,70 @@ class BARS {
 		return (count($arr) > 0);
 	}
 }
+
+class PATTERNS {
+
+    private $virtualDir;
+    private $bars;
+    private $email;
+
+    function __construct() {
+
+        //VIRTUALDIR
+        $this->virtualDir = "/desbravadores/";
+
+        //EMAIL
+        $this->email = "desbravadores@iasd-capaoredondo.com.br";
+        
+        //DEFINICOES DO BARCODE
+        $this->bars = new BARS();
+    }
+
+    public function getClubeDS($p){
+        $str  = in_array("cl",$p) ? "Clube " : "";
+        $str .= in_array("cj",$p) && in_array("db",$p) && !empty($str) ? "de " : "";
+        $str .= in_array("db",$p) ? "Desbravadores " : "";
+        $str .= in_array("nm",$p) ? "Pioneiros " : "";
+        $str .= in_array("sp",$p) && in_array("af",$p) && !empty($str) ? "- " : "";
+        $str .= in_array("af",$p) ? "1959 " : "";
+        $str .= in_array("sp",$p) && in_array("ig",$p) && !empty($str) ? "- " : "";
+        $str .= in_array("ig",$p) ? "IASD Capão Redondo " : "";
+        $str .= in_array("sp",$p) && in_array("rg",$p) && !empty($str) ? "- " : "";
+        $str .= in_array("rg",$p) ? "6ª Região " : "";
+        $str .= in_array("sp",$p) && in_array("as",$p) && !empty($str) ? "- " : "";
+        $str .= in_array("as",$p) ? "APS " : "";
+        $str .= in_array("sp",$p) && in_array("un",$p) && !empty($str) ? "- " : "";
+        $str .= in_array("un",$p) ? "UCB " : "";
+        $str .= in_array("sp",$p) && in_array("dv",$p) && !empty($str) ? "- " : "";
+        $str .= in_array("dv",$p) ? "DSA" : "";
+        return trim($str);
+    }
+
+    //RETORNA DESCRICAO DO CLUBE
+    public function getCDS(){
+        return $this->getClubeDS( array( "cl", "cj", "db", "nm", "sp", "ig", "rg", "as", "un", "dv" ) );
+    }
+
+    public function getMail(){
+        return $this->email;
+    }
+
+    public function getBars(){
+        return $this->bars;
+    }
+
+    //RETORNA VIRTUAL DIR
+    public function getVD(){
+        return $this->virtualDir;
+	}
+	
+	public static function toConvert($n){
+        return base_convert($n,10,36);
+    }
+
+    public static function fromConvert($s){
+        return base_convert($s,36,10);
+    }
+}
+$GLOBALS['pattern'] = new PATTERNS();
 ?>
