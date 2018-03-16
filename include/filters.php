69cbe15be8d8d4bd45fdffbd1ad5ec634c43bfbd
Ricardo Jonadabs César <pexinho@uol.com.br>
2018-03-16 12:32:39 -0300

    New Feature Vegetariano SIM/NÃO

--- include/filters.php
+++ include/filters.php
@@ -53,6 +53,13 @@ function getDomainFilter( $parameters ) {
 			array( "id" => "M", "ds" => "MASCULINO", "icon" => "fas fa-mars" )
 		);
 
+	//VEGETARIANO
+	elseif ( $type == "V" ):
+		$domain = array(
+			array( "id" => "S", "ds" => "SIM", "icon" => "fas fa-thumbs-o-up" ),
+			array( "id" => "N", "ds" => "NÃO", "icon" => "fas fa-thumbs-o-down" )
+		);
+
 	//SITUACAO
 	elseif ( $type == "S" ):
 		$domain = array(
