a2a17c2d2990392cb6a60836de5998dccb695a90
Ricardo Jonadabs César <pexinho@uol.com.br>
2018-04-02 19:45:27 -0300

    Composer Update

--- vendor/adodb/adodb-php/perf/perf-oci8.inc.php
+++ vendor/adodb/adodb-php/perf/perf-oci8.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
-@version   v5.20.9  21-Dec-2016
+@version   v5.20.12  30-Mar-2018
 @copyright (c) 2000-2013 John Lim (jlim#natsoft.com). All rights reserved.
 @copyright (c) 2014      Damien Regad, Mark Newnham and the ADOdb community
   Released under both BSD license and Lesser GPL library license.
@@ -423,7 +423,7 @@ CREATE TABLE PLAN_TABLE (
 		$s = "<p><b>Explain</b>: ".htmlspecialchars($sql)."</p>";
 
 		$this->conn->BeginTrans();
-		$id = "ADODB ".time();
+		$id = "ADODB ".microtime();
 
 		$rs = $this->conn->Execute("EXPLAIN PLAN SET STATEMENT_ID='$id' FOR $sql");
 		$m = $this->conn->ErrorMsg();
