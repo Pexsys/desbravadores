8092e426174193493f402230d14bea5dd7f2cb36
Ricardo Jonadabs César <ricardo@kapmug.com.br>
2018-11-12 15:48:11 -0200

    Material Type

--- dashboard/js/listaCompras.js
+++ dashboard/js/listaCompras.js
@@ -296,8 +296,8 @@ $(document).ready(function(){
 
     $('#divItem').visible(false);
     $('#divCmpl').visible(false);
-    $('#divOQue').visible(true);
-    $('#divTipoMaterial').visible(false);
+    $('#divOQue').visible(false);
+    $('#divTipoMaterial').visible(true);
 		$('#divParaQuem').visible(true);
 		$('#divDataQuando').visible(true);
     jsLIB.resetForm( $('#cadListaForm') );
