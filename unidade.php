58df18e7cad257dc50433f6d03c9f05f9871046e
Ricardo Jonadabs César <pexinho@uol.com.br>
2020-01-14 15:34:39 -0300

    Units Review

--- unidade.php
+++ unidade.php
@@ -5,28 +5,14 @@
 $result = CONN::get()->Execute( "
   SELECT *
     FROM TAB_UNIDADE 
-   WHERE FG_ATIVA = ?
-     AND ID = ?
+   WHERE ID = ?
      AND DESCRIPTION IS NOT NULL
-ORDER BY IDADE, TP", Array( 'S', fRequest("id") ) );
+ORDER BY IDADE, TP", Array( fRequest("id") ) );
 	
 if ($result->EOF):
     header('location: unidades.php');    
 endif;
 fHeaderPage();
-
-$strmembros = "";
-$membros = CONN::get()->Execute( "
-	SELECT * 
-	  FROM CON_ATIVOS 
-	 WHERE ID_UNIDADE = ? 
-	ORDER BY CD_CARGO, NM
-", Array( fRequest("id") ) );
-foreach ($membros as $k => $f):
-    $strmembros .= (empty($strmembros)?"":", ");
-    $a = explode(" ",$f["NM"]);
-    $strmembros .= (fStrStartWith($f["CD_CARGO"],"2")?"<u>".titleCase($a[0])."</u>":titleCase($a[0]));
-endforeach;
 ?>
 <body>
 
@@ -50,38 +36,69 @@ endforeach;
             </div>
         </div>
         <!-- /.row -->
-
-        <!-- Portfolio Item Row -->
-        <div class="row">
-
-            <div class="col-md-4">
-				<img class="img-responsive" src="report/img/unidade/<?php echo $result->fields["ID"];?>_D.png" alt="">
+        
+         <div class="row">
+            <div class="col-lg-8">
+                <div class="row">
+                    <div class="col-md-6">
+        				<img class="img-responsive" src="report/img/unidade/<?php echo $result->fields["ID"];?>_D.png" alt="">
+                    </div>
+                    <div class="col-md-6">
+                        <h3>Atributos</h3>
+                        <ul>
+                            <li><b>Idade</b>:&nbsp;<?php echo $result->fields["IDADE"];?> anos</li>
+                            <li><b>G&ecirc;nero</b>:&nbsp;<?php echo ( $result->fields["TP"] == "F" ? "Feminino" : "Masculino" );?></li>
+                            <li><b>Cor do g&ecirc;nero</b>:&nbsp;<i class="fas fa-stop" aria-hidden="true" style="color:<?php echo $result->fields["CD_COR_GENERO"];?>"></i></li>
+                            <li><b>Cor da unidade</b>:&nbsp;<i class="fas fa-stop" aria-hidden="true" style="color:<?php echo $result->fields["CD_COR"];?>"></i></li>
+                            <li><b>Grito de Guerra</b>:<br/><i><?php echo ($result->fields["GRITO"]);?></i></li>
+                            <?php 
+                            if (!empty($result->fields["HISTORY"])):
+                                echo "<li><b>Hist&oacute;ria e Significados</b>:&nbsp;{$result->fields["HISTORY"]}</li>";
+                            endif;
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+        	            <h3>Caracter&iacute;sticas da Ave Símbolo</h3>
+        	            <?php echo ($result->fields["DESCRIPTION"]);?>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-8">
-                <h3>Atributos da Unidade</h3>
-                <ul>
-                    <li><b>Idade</b>:&nbsp;<?php echo $result->fields["IDADE"];?> anos</li>
-                    <li><b>G&ecirc;nero</b>:&nbsp;<?php echo ( $result->fields["TP"] == "F" ? "Feminino" : "Masculino" );?></li>
-                    <li><b>Cor do g&ecirc;nero</b>:&nbsp;<i class="fas fa-stop" aria-hidden="true" style="color:<?php echo $result->fields["CD_COR_GENERO"];?>"></i></li>
-                    <li><b>Cor da unidade</b>:&nbsp;<i class="fas fa-stop" aria-hidden="true" style="color:<?php echo $result->fields["CD_COR"];?>"></i></li>
-                    <li><b>Grito de Guerra</b>:<br/><i><?php echo ($result->fields["GRITO"]);?></i></li>
-                    <?php 
-                    if (strlen($strmembros)):
-                        echo "<li><b>{$membros->RecordCount()}&nbsp;Membros</b>:&nbsp;$strmembros</li>";
+            <div class="col-lg-4">
+                <h3>Fazendo história</h3>
+                <ul class="list-group">
+                <?php
+                $anoAnt = "";
+                $strmembros = "";
+                $anos = CONN::get()->Execute("
+                    SELECT ca.NR_ANO, ca.CD_CARGO, cp.NM
+                	  FROM CAD_ATIVOS ca
+                      INNER JOIN CAD_MEMBRO cm ON (ca.ID_CAD_MEMBRO = cm.ID)
+                      INNER JOIN CAD_PESSOA cp ON (cp.ID = cm.ID_CAD_PESSOA)
+                	 WHERE ca.ID_UNIDADE = ?
+                	ORDER BY ca.NR_ANO DESC, ca.CD_CARGO, cp.NM
+                ", Array( fRequest("id") ) );
+                foreach ($anos as $k => $f):
+                    $color = $f["NR_ANO"] == date('Y') ? " style=\"background-color:#28a745\"" : "";
+                    if ($anoAnt != $f["NR_ANO"]):
+                        if ($strmembros != ""):
+                            echo "$strmembros</li>";
+                            $strmembros = "";
+                        endif;
+                        echo "<li class=\"list-group-item\"><span class=\"badge pull-left badge-success\"$color>".$f["NR_ANO"]."</span>&nbsp;";
+                        $anoAnt = $f["NR_ANO"];
                     endif;
-                    if (!empty($result->fields["HISTORY"])):
-                        echo "<li><b>Hist&oacute;ria e Significados</b>:&nbsp;{$result->fields["HISTORY"]}</li>";
-                    endif;
-                    ?>
+                    $strmembros .= (empty($strmembros)?"":", ");
+                    $a = explode(" ",$f["NM"]);
+                    $strmembros .= (fStrStartWith($f["CD_CARGO"],"2")?"<u>".titleCase($a[0])."</u>":titleCase($a[0]));
+                endforeach;
+                echo "$strmembros</li>";
+                ?>
                 </ul>
             </div>
-            <div class="col-md-12">
-	            <h3>Caracter&iacute;sticas da Ave</h3>
-	            <?php echo ($result->fields["DESCRIPTION"]);?>
-            </div>
-
         </div>
-        <!-- /.row -->
 
         <!-- Related Projects Row -->
         <div class="row">
@@ -94,11 +111,10 @@ endforeach;
             $result = CONN::get()->Execute( "
         	  SELECT *
         	    FROM TAB_UNIDADE 
-        	   WHERE FG_ATIVA = ?
-        	     AND TP IN ('F','M')
+        	   WHERE TP IN ('F','M')
         	     AND ID <> ?
         	  ORDER BY ID
-            ", Array( 'S', $result->fields["ID"] ) );
+            ", Array( $result->fields["ID"] ) );
             
             $a = array();
             foreach ($result as $ln):
