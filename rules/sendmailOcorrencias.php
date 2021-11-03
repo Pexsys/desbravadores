8ccfbaee74d506c7ad215412438f68f3c3b81f53
Pexinho <pexinho@uol.com.br>
2021-11-03 12:01:50 -0300

    fix send comunicados / ocorrencias

--- rules/sendmailOcorrencias.php
+++ rules/sendmailOcorrencias.php
@@ -1,49 +1,28 @@
 <?php
-$mail = MAIL::get();
 function sendOcorrenciaByID($ocorrenciaID, $nrEnviados){
 	$rs = CONN::get()->Execute("
-			SELECT DISTINCT co.ID, co.CD, co.DH, co.TXT
-			  FROM LOG_MENSAGEM lc
-		INNER JOIN CAD_OCORRENCIA co ON (co.ID = lc.ID_ORIGEM AND lc.TP = 'O')
-			 WHERE co.ID = ?
- 			   AND co.FG_PEND = 'N'
-			   AND lc.DH_SEND IS NULL
-			   AND lc.EMAIL IS NOT NULL
-		  ORDER BY 1
+		SELECT DISTINCT co.CD, co.DH, co.TXT, lc.ID, lc.EMAIL
+		  FROM LOG_MENSAGEM lc
+	INNER JOIN CAD_OCORRENCIA co ON (co.ID = lc.ID_ORIGEM AND lc.TP = 'O')
+		 WHERE co.ID = ?
+ 		   AND co.FG_PEND = 'N'
+		   AND lc.DH_SEND IS NULL
+		   AND lc.EMAIL IS NOT NULL
+	  ORDER BY 1
 	", array($ocorrenciaID) );
 	if (!$rs->EOF):
-    $mail = MAIL::get();
-    $mail->Subject = PATTERNS::getClubeDS( array("cl","nm") ) . " - Ocorrencia #".$l["CD"]." [".strftime("%d/%m/%Y",strtotime($l["DH"])) ."]";
-    
-		$rs1 = CONN::get()->Execute("
-			SELECT *
-			  FROM LOG_MENSAGEM
-			 WHERE DH_SEND IS NULL
-			   AND EMAIL IS NOT NULL
-			   AND ID_ORIGEM = ?
-			   AND TP = ?
-	      ORDER BY ID
-		", array($ocorrenciaID, "O") );
-		if ( !$rs1->EOF ):
-			foreach($rs1 as $k1 => $l1):
-				$mail->ClearAllRecipients();
-				$mail->AddAddress($l1["EMAIL"]);
-				$mail->MsgHTML($l["TXT"]);
-					
-        if ( $mail->Send() ):
-          ++$nrEnviados;
-					CONN::get()->Execute("
-						UPDATE LOG_MENSAGEM
-						   SET DH_SEND = NOW()
-						 WHERE ID = ?
-					", array($ocorrenciaID) );
-				else:
-					echo "email não enviado para ". $l1["EMAIL"]."<br/>";
-				endif;
-			
-			endforeach;
-		endif;
+	    $mail = MAIL::get();
+		foreach($rs as $k => $l):
+		    $mail->Subject = PATTERNS::getClubeDS( array("cl","nm") ) . " - Ocorrencia #".$l["CD"]." [".strftime("%d/%m/%Y",strtotime($l["DH"])) ."]";
+		    $mail->ClearAllRecipients();
+			$mail->AddAddress($l["EMAIL"]);
+			$mail->MsgHTML($l["TXT"]);
+            $mail->Send();
+            ++$nrEnviados;
+			CONN::get()->Execute("UPDATE LOG_MENSAGEM SET DH_SEND = NOW() WHERE ID = ?", array($l["ID"]) );
+	    endforeach;
   endif;
+
   return $nrEnviados;
 }
 ?>
