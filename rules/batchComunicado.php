d4170bdc3d6e59e62349b63b7729da6f2cbe9bc9
Pexinho <pexinho@uol.com.br>
2021-11-03 12:01:50 -0300

    fix send comunicados / ocorrencias

--- rules/batchComunicado.php
+++ rules/batchComunicado.php
@@ -2,9 +2,11 @@
 @require_once("../include/functions.php");
 @require_once("sendmailOcorrencias.php");
 
+$mail = MAIL::get();
+
 //SECRETARIA - COMUNICADOS - EMAILS NAO ENVIADOS
 $nrEnviados = 0;
-CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.00-Analisando comunicados não enviados...')");
+CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.01-Analisando comunicados não enviados...')");
 $rs = CONN::get()->Execute("
 		SELECT DISTINCT cc.ID, cc.CD, cc.DH, cc.TXT
 		  FROM LOG_MENSAGEM lc
@@ -25,35 +27,32 @@ foreach($rs as $k => $l):
 		   AND TP = ?
       ORDER BY ID
 	", array($l["ID"], "C") );
-  if ( !$rs1->EOF ):
-    
+    if ( !$rs1->EOF ):
 		$mail->ClearAllRecipients();
 		foreach($rs1 as $k1 => $l1):
-			//$mail->AddAddress();
-			$mail->addBCC( $l1["EMAIL"] );
+			$mail->addBCC($l1["EMAIL"]);
 			$nrEnviados++;
 		endforeach;
-
 		$mail->MsgHTML($l["TXT"]);
-    $mail->Send();
+        $mail->Send();
     
 		//ATUALIZA ENVIO
 		CONN::get()->Execute("
 			UPDATE LOG_MENSAGEM
-				SET DH_SEND = NOW()
-				WHERE DH_SEND IS NULL
-				AND EMAIL IS NOT NULL
-				AND ID_ORIGEM = ?
-				AND TP = ?
+			SET DH_SEND = NOW()
+			WHERE DH_SEND IS NULL
+			AND EMAIL IS NOT NULL
+			AND ID_ORIGEM = ?
+			AND TP = ?
 		", array( $l["ID"], "C") );
 	endif;
 endforeach;
-CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.99-Comunicados enviados com sucesso ($nrEnviados)...')");
+CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.02-Comunicados enviados com sucesso ($nrEnviados)...')");
 
 
 //SECRETARIA - OCORRENCIAS - EMAILS NAO ENVIADOS
 $nrEnviados = 0;
-CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.00-Analisando ocorrencias não enviados...')");
+CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.03-Analisando ocorrencias não enviados...')");
 $rs = CONN::get()->Execute("
 		SELECT DISTINCT co.ID, co.CD, co.DH, co.TXT
 		  FROM LOG_MENSAGEM lc
@@ -66,7 +65,7 @@ $rs = CONN::get()->Execute("
 foreach ($rs as $l):
 	$nrEnviados = sendOcorrenciaByID($l["ID"], $nrEnviados);
 endforeach;
-CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.99-Ocorrencias enviados com sucesso ($nrEnviados)...')");
+CONN::get()->Execute("INSERT INTO LOG_BATCH(TP,DS) VALUES('EMAILS','01.04-Ocorrencias enviadas com sucesso ($nrEnviados)...')");
 
 exit;
 ?>
